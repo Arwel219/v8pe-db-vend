8534c80194b1e77e60ed4d645cfd39860ed05b8e
Arwel219 <78668254+Arwel219@users.noreply.github.com>
2025-05-19 15:06:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Inserisci le tue credenziali
-const supabaseUrl = 'https://cynshbqlpgghjgwltukp.supabase.co'; // trova nella dashboard
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bnNoYnFscGdnaGpnd2x0dWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTY4MjIsImV4cCI6MjA2MzIzMjgyMn0.uJ0VMtAoqawydknYNwdwQxGsvdSPW49Y36Seo9WNCQ8'; // key pubblica
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-
 async function salvaUtente(nome, vendite) {
   // Verifica se l'utente esiste
   const { data: existingUser, error } = await supabase
