80f8a890b371cd36d2231cd29d221302744ae8f0
Arwel219 <78668254+Arwel219@users.noreply.github.com>
2025-05-19 19:49:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const supabase = createClient(
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bnNoYnFscGdnaGpnd2x0dWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTY4MjIsImV4cCI6MjA2MzIzMjgyMn0.uJ0VMtAoqawydknYNwdwQxGsvdSPW49Y36Seo9WNCQ8'
 );
 
+console.log('Supabase Instance: ', supabase);
+
 async function gestisciResetMensile() {
   const today = new Date();
   const giorno = today.getDate();
