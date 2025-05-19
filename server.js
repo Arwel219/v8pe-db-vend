e25c72de9786b21b96ac2d6103d46527c425352d
Arwel219 <78668254+Arwel219@users.noreply.github.com>
2025-05-19 16:11:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-// Inserisci le tue credenziali
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+  const { createClient } = supabase
+  const _supabase = createClient('https://cynshbqlpgghjgwltukp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bnNoYnFscGdnaGpnd2x0dWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTY4MjIsImV4cCI6MjA2MzIzMjgyMn0.uJ0VMtAoqawydknYNwdwQxGsvdSPW49Y36Seo9WNCQ8')
 
-const supabaseUrl = 'https://cynshbqlpgghjgwltukp.supabase.co'; // trova nella dashboard
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5bnNoYnFscGdnaGpnd2x0dWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NTY4MjIsImV4cCI6MjA2MzIzMjgyMn0.uJ0VMtAoqawydknYNwdwQxGsvdSPW49Y36Seo9WNCQ8'; // key pubblica
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+  console.log('Supabase Instance: ', _supabase)
